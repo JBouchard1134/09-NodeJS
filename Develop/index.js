6c63c7a5b4f078a8ad5a8d578edb73907d805dce
Jeremy Bouchard <JBouchard1134@gmail.com>
2020-09-29 02:18:07 -0700

    Added GitHub API to prompt and verify username

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,35 @@
-const questions = [
+const axios = require("axios");
+const inquirer = require("inquirer");
 
+//Global Object Definition
+const questions = [
+    {
+        type: "input",
+        message: "Enter your GitHub username?",
+        name: "username"
+    }
 ];
 
-function writeToFile(fileName, data) {
-}
-
+// Initialize with questions to user
 function init() {
-
-}
+    inquirer
+        .prompt(questions)
+        .then(function (input) {
+            // console.log(answers);
+            username = input.username;
+            const queryUrl = `https://api.github.com/users/${username}`;
+            return queryUrl;
+        })
+        .then(function (queryUrl) {
+            axios.get(queryUrl)
+                .then(function () {
+                    console.log(`Success!`);
+                })
+                .catch(function (error) {
+                    console.log("Please enter a valid Github username", error);
+                    return;
+                });
+        });
+};
 
 init();
